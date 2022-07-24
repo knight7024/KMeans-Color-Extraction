c850917e32778745132fae3e44c60e70d41763b0
Jong Woo Jeong <night08241@gmail.com>
2022-07-24 20:00:43 +0900

    feat: CIELAB 색 공간 추가, 추상적인 색상명으로 변경

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import warnings
 from ast import literal_eval
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from colour import delta_E, sRGB_to_XYZ, XYZ_to_Lab
 from kneed import KneeLocator
 from sklearn.cluster import KMeans
 
 warnings.filterwarnings("ignore")
+
+color_name_list = ['black', 'blue', 'brown', 'gray', 'green', 'orange', 'pink', 'red', 'purple', 'yellow', 'white']
 
 max_k = 5
 
@@ -36,34 +40,40 @@ def get_best_k(points):
 
 
 # 색상 추출
-# Compared with Avery Colorbook from colornerd project (https://github.com/jpederson/colornerd)
-def extract_colors(kmeans: KMeans, csv_name: str) -> list:
-    extracted_colors = []
-    data = pd.read_csv(csv_name, encoding='utf-8-sig')
+def extract_colors(kmeans: KMeans, csv_name: str = None) -> list:
+    ret_colors = []
 
-    def color_distance(x: tuple, y: tuple) -> float:
-        assert len(x) == 3 and len(y) == 3
-        r_mean = (x[0] + y[0]) // 2
-        r, g, b = abs(x[0] - y[0]), abs(x[1] - y[1]), abs(x[2] - y[2])
-        # Colour Distance Algorithm (https://www.compuphase.com/cmetric.htm)
-        return ((((512 + r_mean) * r * r) >> 8) + (4 * g * g) + (((767 - r_mean) * b * b) >> 8)) ** 0.5
+    # data = pd.read_csv(csv_name, encoding='utf-8-sig')
+
+    # def color_distance_RGB(x: tuple, y: tuple):
+    #     assert len(x) == len(y) == 3
+    #     r_mean = (x[0] + y[0]) // 2
+    #     r, g, b = x[0] - y[0], x[1] - y[1], x[2] - y[2]
+    #     # Colour Distance Algorithm (https://www.compuphase.com/cmetric.htm)
+    #     return (((512 + r_mean) * r * r) >> 8) + (4 * g * g) + (((767 - r_mean) * b * b) >> 8)
+
+    def color_distance_LAB(x: tuple, y: tuple):
+        assert len(x) == len(y) == 3
+        x_space = XYZ_to_Lab(sRGB_to_XYZ(np.array([x[0] / 255, x[1] / 255, x[2] / 255])))
+        y_space = XYZ_to_Lab(sRGB_to_XYZ(np.array([y[0] / 255, y[1] / 255, y[2] / 255])))
+
+        return delta_E(a=x_space, b=y_space, method='CIE 2000')
 
     for color in kmeans.cluster_centers_:
         rgb_from = tuple(color[:3].astype("uint8"))
 
         min_dist = float('inf')
         final_color_name = ''
-        for i in range(len(data)):
-            row = data.loc[i]
-            rgb_to, color_name = literal_eval(row[1]), row[0]
+        for rgb_to in color_names_dictionary:
+            color_name = color_names_dictionary[rgb_to]
 
-            dist = color_distance(rgb_from, rgb_to)
+            dist = color_distance_LAB(rgb_from, rgb_to)
             if dist < min_dist:
                 min_dist = dist
                 final_color_name = color_name
 
-        extracted_colors.append(final_color_name)
-    return extracted_colors
+        ret_colors.append(final_color_name)
+    return ret_colors
 
 
 def load_image(name):
@@ -78,14 +88,26 @@ def load_image(name):
     return image_array
 
 
+def init():
+    global color_names_dictionary
+    color_names_dictionary = {}
+    for color_name in color_name_list:
+        load_csv = pd.read_csv(f'colors/{color_name}_color_list.csv', encoding='utf-8-sig')
+        for i in range(len(load_csv)):
+            row = load_csv.loc[i]
+            rgb = literal_eval(row[1])
+            color_names_dictionary[rgb] = color_name
+
+
 if __name__ == '__main__':
+    init()
     image_array = load_image('463655_1_220_(1).png')
 
     kn = get_best_k(image_array)
-    kmeans = KMeans(n_clusters=kn.knee, random_state=0).fit(image_array)
+    kmeans = KMeans(n_clusters=kn.knee if kn.knee else 1).fit(image_array)
     labels = kmeans.predict(image_array)
 
-    extracted_colors = extract_colors(kmeans=kmeans, csv_name='avery_color_list.csv')
+    extracted_colors = extract_colors(kmeans=kmeans)
     print(extracted_colors)
 
 
@@ -119,7 +141,6 @@ if __name__ == '__main__':
             endX = startX + (percent * 300)
             cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
                           color.astype("uint8").tolist(), -1)
-            # print(color[:3].astype("uint8"))
             startX = endX
 
         # return the bar chart
